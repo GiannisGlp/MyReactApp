35bcd670f5c292d2ab041de402672eeae8412af5
Vano Natobaidze <giannisglp@gmail.com>
2021-02-21 20:12:09 +0200

    header fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import "./App.css";
-import Particles from "react-particles-js";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 30,
-              density: {
-                enable: true,
-                value_area: 900,
-              },
-            },
-          },
-        }}
-      />
       <Header />
     </>
   );
